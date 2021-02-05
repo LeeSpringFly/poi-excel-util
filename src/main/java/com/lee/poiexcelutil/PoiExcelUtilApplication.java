484d5b2f9d1cf1cc37cc50243b87f0c6ed959ffa
lipengchun <772890143@qq.com>
2021-02-05 15:21:30 +0800

    初始化项目

---
+++ src/main/java/com/lee/poiexcelutil/PoiExcelUtilApplication.java
@@ -0,0 +1,13 @@
+package com.lee.poiexcelutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiExcelUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoiExcelUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lee/poiexcelutil/PoiExcelUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.poiexcelutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiExcelUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
